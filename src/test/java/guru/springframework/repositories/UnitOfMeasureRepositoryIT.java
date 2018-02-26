a8fb3fb45f7aa6b26705ef48c26317439c892cb2
jklancic <jernej.klancic@appdirect.com>
2018-02-25 18:21:29 -0700

    Section 17 Lecture 223

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,31 +1,43 @@
 package guru.springframework.repositories;
 
-import guru.springframework.domain.UnitOfMeasure;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.bootstrap.RecipeBootstrap;
+import guru.springframework.domain.UnitOfMeasure;
 
 /**
  * Created by jt on 6/17/17.
  */
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
+    @Autowired
+    CategoryRepository categoryRepository;
+    
+    @Autowired
+    RecipeRepository recipeRepository;
+    
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
     @Before
     public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        categoryRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        
+        RecipeBootstrap bootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        bootstrap.onApplicationEvent(null);
     }
 
     @Test
